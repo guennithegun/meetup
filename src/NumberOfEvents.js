e1256e1b5fa1a66c155110dcd4e8db869c781d4c
Günther von Häfen <>
2019-09-11 22:27:04 +0200

    4.4 Changes on NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NumberOfEvents extends Component {
 
   state = {
-    numberOfEvents: this.props.numberOfEvents
+    numberOfEvents: 1
   }
 
   handleInputChanged = (event) => {
