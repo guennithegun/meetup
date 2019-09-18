8e901431c47843e1f3ce9f65822893a9868491dd
Günther von Häfen <>
2019-09-18 22:24:05 +0200

    Updates

--- precache-manifest.637c5fc3c6409a710571c5aea8298ea2.js
+++ precache-manifest.637c5fc3c6409a710571c5aea8298ea2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5f6579c6538907d45d65f544019c546",
+    "revision": "4abcd73752207b9f2294e4c1f1017a62",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "5f22bf1d2dd34c81b570",
+    "revision": "f59608ce7f13a21c9e98",
     "url": "/meetup/static/css/main.b9b5a123.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.7741fb28.chunk.js"
   },
   {
-    "revision": "5f22bf1d2dd34c81b570",
-    "url": "/meetup/static/js/main.3d03d25c.chunk.js"
+    "revision": "f59608ce7f13a21c9e98",
+    "url": "/meetup/static/js/main.39bb0a30.chunk.js"
   },
   {
     "revision": "c81de03e5fcc2eb84eed",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.e393b3f9d943d2b5b0a1f9810fb97029.js"
+  "/meetup/precache-manifest.637c5fc3c6409a710571c5aea8298ea2.js"
 );
 
 self.addEventListener('message', (event) => {
